84a0a634ee49a28ca2bfec6651b24f7d11cd4ba6
E33-a <emax03736@gmail.com>
2025-11-01 23:51:36 -0600

    feat: Instalación de font awesome, adición de icono svg para la pantalla de login en assets, login screen creada como stateful (aún sin funcionalidad)

--- lib/features/2_auth/presentation/screens/login_screen.dart
+++ lib/features/2_auth/presentation/screens/login_screen.dart
@@ -1,16 +1,148 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
 
   @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final _emailController = TextEditingController();
+  final _passwordController= TextEditingController();
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final textTheme = Theme.of(context).textTheme;
+    final colorScheme = Theme.of(context).colorScheme;
+
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Attune - Login'),
-      ),
-      body: const Center(
-        child: Text('Pantalla de Login'),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 24.0, vertical: 48),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                SvgPicture.asset(
+                  'assets/icon/icon.svg',
+                  width: 100.0,
+                  height: 100.0,
+                ),
+                const SizedBox(height: 32),
+
+                Text(
+                  '¡Bienvenido!',
+                  textAlign:  TextAlign.center,
+                  style: textTheme.headlineMedium?.copyWith(
+                    fontWeight: FontWeight.bold,
+                    color: colorScheme.onSurface,
+                  ),
+                ),
+                const SizedBox(height: 48),
+
+                TextFormField(
+                  controller: _emailController,
+                  decoration: const InputDecoration(labelText: 'Correo'),
+                  keyboardType: TextInputType.emailAddress,
+                  autocorrect: false,
+                ),
+                const SizedBox(height: 16),
+
+                TextField(
+                  controller: _passwordController,
+                  decoration: const InputDecoration(labelText: 'Contraseña'),
+                  obscureText: true,
+                ),
+                const SizedBox(height: 16),
+
+                Align(
+                  alignment: Alignment.centerRight,
+                  child: TextButton(
+                    onPressed: () {
+                      // Olvidaste la contraseña (falta implementar)
+                    }, 
+                    child: const Text('¿Olvidaste tu contraseña?'),
+                  ),
+                ),
+                const SizedBox(height: 24),
+
+                ElevatedButton(
+                  onPressed: ()
+                  {
+                    // TODO: Implementar lógica de login con Email/Pass
+                    // String email = _emailController.text;
+                    // String password = _passwordController.text;
+                  }, 
+                  style: ElevatedButton.styleFrom(
+                    padding: const EdgeInsets.symmetric(vertical: 16),
+                  ),
+                  child: const Text(
+                    'Ingresar',
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 32),
+
+                Row(
+                  children: [
+                    const Expanded(child: Divider()),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                      child: Text(
+                        'o también',
+                        style: textTheme.bodySmall?.copyWith(
+                          color: colorScheme.onSurfaceVariant,
+                        ),
+                      ),
+                    ),
+                    const Expanded(child: Divider()),
+                  ],
+                ),
+                const SizedBox(height: 32),
+
+                OutlinedButton.icon(
+                  onPressed: () {
+                    // TODO: Implementar lógica de login con Google
+                  },
+                  icon: const FaIcon(FontAwesomeIcons.google, color: Colors.red),
+                  label: const Text('Ingresar con Google'),
+                  style: OutlinedButton.styleFrom(
+                    padding: const EdgeInsets.symmetric(vertical: 12),
+                    foregroundColor: colorScheme.onSurface,
+                  ),
+                ),
+                const SizedBox(height: 12),
+
+                OutlinedButton.icon(
+                  onPressed: () {
+                    // TODO: Implementar lógica de login con Google
+                  },
+                  icon: const FaIcon(FontAwesomeIcons.facebook, color: Colors.blue),
+                  label: const Text('Ingresar con Facebook'),
+                  style: OutlinedButton.styleFrom(
+                    padding: const EdgeInsets.symmetric(vertical: 12),
+                    foregroundColor: colorScheme.onSurface,
+                  ),
+                ),
+                  // SI SE REQUIEREN MAS METODOS ADICIONAR AQUI
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
